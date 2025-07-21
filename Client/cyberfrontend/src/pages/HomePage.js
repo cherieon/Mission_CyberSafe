d5830600e5d69af14e45daedefec329988206115
Leen <Leena.kraytem@gmail.com>
2025-07-21 17:46:38 -0400

    created home page

---
+++ Client/cyberfrontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to My Home Page!</h1>
+      <p>This is the content of the homepage.</p>
+    </div>
+  );
+}
+
+export default HomePage;
